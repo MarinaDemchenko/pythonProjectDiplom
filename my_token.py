a62fc26592637dfdb9c737ed9a58546bf583f760
MarinaDemchenko <116454347+MarinaDemchenko@users.noreply.github.com>
2022-11-08 20:28:16 +0300

    Delete my_token.py

--- my_token.py
+++
@@ -1,2 +0,0 @@
-token_vk = 'vk1.a.dF1MEZDjWt0UzgHHLGdvNyJA955FaOjHE-pFhqSvokTXpy8fpmonOgrlAnRpy6Yn-keKmrzgoJlC8yfXzSZ-ONV5FibEpyKTdPYVD4WRxPrqqotAie2sowxDax-r14mkzK5Sgmhay6lfuhQHkldoUCagx5Um23ha3mIQOnMsb2MM6bkocueuzPW3fGSRHSwW'
-token_ya = 'y0_AgAAAAAR5mVDAADLWwAAAADSw0QW6t6CnRJhRzyRLyVUjxZoviuiUsE'
